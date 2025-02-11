b2a31aa350352387bcf24910f2af5dcedbde1623
Adrian Cosma <cosma.i.adrian@gmail.com>
2025-02-11 19:08:42 +0200

    added basic reasoning prompt

---
+++ evaluate/prompts/romanian_reasoning_prompt.py
@@ -0,0 +1,11 @@
+PROMPT = [
+    {
+        "role": "system",
+        "content":
+        """Răspunde in limba română, in următorul format: <raționament>...</raționament><răspuns>...</răspuns>""",
+    },
+    {
+        "role": "user",
+        "content": """{problem_statement}""",
+    }
+]
